fcc5dbdee6a43f306a51c0bd904f07cb0086e23c
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2024-12-19 17:03:19 +0700

    :zap: Auto close browser when nitro is closed

--- server/routes/invoice.pdf.ts
+++ server/routes/invoice.pdf.ts
@@ -1,11 +1,25 @@
 import puppeteer from "puppeteer";
 
 export default defineEventHandler(async (event) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
+  const { page } = await usePuppeteer()
   await page.goto(`${getRequestURL(event).origin}/_invoice`)
 
   setHeader(event, 'Content-Type', 'application/pdf')
   return page.pdf({ format: 'A4' })
 });
+
+async function usePuppeteer() {
+  const nitroApp = useNitroApp()
+  
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  nitroApp.hooks.hook('close', async () => {
+    browser.close()
+  })
+
+  return {
+    browser,
+    page,
+  }
+}
